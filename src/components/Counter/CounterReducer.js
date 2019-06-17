1ea6901a503b101b6b6e07f4e724435eefae8159
Ignacio Castañeda <icastanedaw@gmail.com>
2019-06-16 22:03:02 -0400

    removed counter console log

--- src/components/Counter/CounterReducer.js
+++ src/components/Counter/CounterReducer.js
@@ -3,7 +3,6 @@ import { database } from "config";
 const initialState = 0;
 
 export const reducer = (state = initialState, action) => {
-	console.log(action);
 	const { type, payload } = action;
 	switch (type) {
 		case "SET_COUNTER":
