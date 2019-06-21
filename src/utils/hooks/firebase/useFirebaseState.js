4fc7a67f1cb6432ffa64192e052fe92d537de2d6
Ignacio Castañeda <icastanedaw@gmail.com>
2019-06-21 17:50:25 -0400

    deep-equal

--- src/utils/hooks/firebase/useFirebaseState.js
+++ src/utils/hooks/firebase/useFirebaseState.js
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 import { useObjectVal } from "react-firebase-hooks/database";
+import deepEqual from "deep-equal";
 import { getDatabaseRef } from "utils";
 
 export default (refPath, state, setState) => {
 	const ref = getDatabaseRef(refPath);
 	const [value, loading, error] = useObjectVal(ref);
 	useEffect(() => {
-		!loading && value !== state && setState(value);
+		!loading && !deepEqual(value, state) && setState(value);
 	}, [value, loading, setState, state]);
 	return [loading, error];
 };
