7e93368f8569633ce4c0d183593d817df7f29917
Ignacio Castañeda <icastanedaw@gmail.com>
2019-09-07 16:50:31 -0400

    fixed bug that happened when state was null

--- src/utils/hooks/firebase/useFirebaseState.js
+++ src/utils/hooks/firebase/useFirebaseState.js
@@ -3,11 +3,12 @@ import { useObjectVal } from "react-firebase-hooks/database";
 import deepEqual from "deep-equal";
 import { getDatabaseRef } from "utils";
 
-export default (refPath, state, setState) => {
-	const ref = getDatabaseRef(refPath);
-	const [value, loading, error] = useObjectVal(ref);
-	useEffect(() => {
-		!loading && !deepEqual(value, state) && setState(value);
-	}, [value, loading, setState, state]);
-	return [loading, error];
+export default (refPath, state, setState, sync = true, initialState = {}) => {
+    const ref = getDatabaseRef(refPath);
+    const [value, loading, error] = useObjectVal(ref);
+    useEffect(() => {
+        const v = value || initialState;
+        sync && !loading && !deepEqual(v, state) && setState(v);
+    }, [value, sync, loading, setState, state, initialState]);
+    return [loading, error];
 };
